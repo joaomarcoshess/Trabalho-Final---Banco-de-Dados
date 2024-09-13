6091db03e53702b255d942cb7c60f4092f2a34d3
Leandro Sousa Costa <118322766+LeannonS@users.noreply.github.com>
2024-09-13 01:29:10 -0300

    Update func_verificacoes.py

--- func_verificacoes.py
+++ func_verificacoes.py
@@ -16,12 +16,6 @@ def verificar_patogeno(cursor, nome_patogeno):
     count = cursor.fetchone()[0]
     return count > 0
 
-# Verifica se o popular já existe a partir de seu nome
-def verificar_nome_popular(cursor, nome_popular):
-    cursor.execute('SELECT COUNT(*) FROM NomesPopulares WHERE nomes_populares = ?', [nome_popular])
-    count = cursor.fetchone()[0]
-    return count > 0
-
 # Verifica se o tipo de patógeno já existe a partir de seu nome
 def verificar_tipo_patogeno(cursor, tipo_patogeno):
     cursor.execute('SELECT COUNT(*) FROM TipoDePatogeno WHERE tipo = ?', [tipo_patogeno])
@@ -31,5 +25,11 @@ def verificar_tipo_patogeno(cursor, tipo_patogeno):
 # Verifica se o CID já existe
 def verificar_cid_existe(cursor, cid):
     cursor.execute('SELECT COUNT(*) FROM Doenca WHERE cid = ?', [cid])
+    count = cursor.fetchone()[0]
+    return count > 0
+
+# Verifica se o popular já existe a partir de seu nome
+def verificar_nome_popular(cursor, nome_popular):
+    cursor.execute('SELECT COUNT(*) FROM NomesPopulares WHERE nomes_populares = ?', [nome_popular])
     count = cursor.fetchone()[0]
     return count > 0
